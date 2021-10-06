38fae3bbb94c285f84ce726ec729bf5d78d7896d
Ah Uzzol <uzzolah@gmail.com>
2021-10-07 01:39:28 +0600

    Time sensitive delivery
    front-end added.

---
+++ resources/views/user/pages/services/time-sensitive-delivery.blade.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Title-->
+    <title>Time Sensitive Delivery</title>
+    @include('user.partials.head')
+</head>
+
+<body>
+    <!----------------------------------------top area start---------------------------------------->
+    @include('user.partials.nav')
+    <!----------------------------------------top area end---------------------------------------->
+
+
+    <!----------------------------------------main area start---------------------------------------->
+    <main class="main-area">
+
+        <!--banner area start-->
+        <section class="top-header Vision position-relative" style="background-image: url(frontend-asstets/assets/image/time-sensitive-delivery/1.png);height: 600px;">
+            <div class="Vision-banner">
+                <h1 data-aos="zoom-in" data-aos-duration="2000">Time Sensitive Delivery</h1>
+            </div>
+
+        </section>
+        <!--banner area end-->
+
+        <!--why skrillaz area start-->
+        <section class="pt-100 pb-100">
+            <!--Tittle-area-start-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="title-main mb-50">
+                            <h1>About Time Sensitive Delivery</h1>
+                            <img data-aos="zoom-in" data-aos-duration="2000" src="{{asset('frontend-asstets/assets/image/banner/tittle.svg')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="next-day">
+                        <p>For many businesses, time-sensitive shipments are essential. They are largely used for high-value products in a bid to minimize theft and damage during transit. Time-sensitive deliveries are there to improve the experience for
+                            customers, they play a major role in business. The ability to efficiently offer this service can take you a landslide ahead of the competitors. And
+                            <a href="https://skrillazlogistics.com/">we're here to make it easy.</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!--Tittle-area-end-->
+        </section>
+        <!--why skrillaz area end-->
+        <section class="pt-100 pb-100 bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="title-main mb-50">
+                            <h1>Why Our Time-Sensitive Delivery Is So Important To <br> Your Business?</h1>
+                            <img data-aos="zoom-in" data-aos-duration="2000" src="{{asset('frontend-asstets/assets/image/banner/tittle.svg')}}" alt="">
+                        </div>
+                        <div class="title-short">
+                            <p>Whether this service is relevant to you or largely depends on the business model you run and the products you sell. There are a host of benefits to consider us:</p>
+                        </div>
+                    </div>
+                </div>
+                <!--time-sensitive-box-start--->
+                <div class="row small-ts-box d-flex align-items-center mt-50 mb-50">
+                    <div class="col-xl-6 col-md-8 col-lg-8 ">
+                        <div class="time-sensitive-info-left">
+                            <h2>
+                                FASTER DELIVERY TIMES
+                            </h2>
+                            <p>
+                                We offer the fastest delivery that are most time sensitive. Being able to offer shorter delivery lead times gives you a major benefit in the face of some heavy competition. It gives the customer additional choice to shop with you for last minute purchases
+                                and considered buying.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6  col-md-4 col-lg-4 ">
+                        <div class="time-sensitive-info-right">
+                            <img src="{{asset('frontend-asstets/assets/image/time-sensitive-delivery/FASTER-DELIVERY-TIMES.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+                <!--time-sensitive-box-end--->
+                <!--time-sensitive-box-start--->
+                <div class="row small-ts-box d-flex align-items-center mt-50 mb-50">
+
+                    <div class="col-xl-6 col-md-4 col-lg-4">
+                        <div class="time-sensitive-info-right">
+                            <img src="{{asset('frontend-asstets/assets/image/time-sensitive-delivery/GUARANTEED-DELIVERY.png')}}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-xl-6 col-md-8 col-lg-8">
+                        <div class="time-sensitive-info-left">
+                            <h2>
+                                GUARANTEED DELIVERY
+                            </h2>
+                            <p>
+                                This service comes at a premium for a reason. Customers pay to secure an exact, guaranteed delivery time. In doing so, the benefits vastly outweigh additional costs. We ensured the delivery in due time.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--time-sensitive-box-end--->
+                <!--time-sensitive-box-start--->
+                <div class="row small-ts-box d-flex align-items-center mt-50 mb-50">
+                    <div class="col-xl-6 col-xl-6 col-md-8 col-lg-8">
+                        <div class="time-sensitive-info-left">
+                            <h2>
+                                REDUCED RISK OF DAMAGE
+                            </h2>
+                            <p>
+                                Traditional transport involves cases when parcels are held for a specified period in a specific facility before they are transferred to the customer. This increases the risk of loss or damage. Time-sensitive shipments reduce the amount of time a parcel
+                                is in the possession of a third-party, thereby the risk of damage also decreases.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-md-4 col-lg-4">
+                        <div class="time-sensitive-info-right">
+                            <img src="{{asset('frontend-asstets/assets/image/time-sensitive-delivery/REDUCED-RISK-OF-DAMAGE.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+                <!--time-sensitive-box-end--->
+                <!--time-sensitive-box-start--->
+                <div class="row small-ts-box d-flex align-items-center mt-50 mb-50">
+
+                    <div class="col-xl-6 col-md-4 col-lg-4">
+                        <div class="time-sensitive-info-right">
+                            <img src="{{asset('frontend-asstets/assets/image/time-sensitive-delivery/USE-SMART-TECHNOLOGY.png')}}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-xl-6 col-md-8 col-lg-8">
+                        <div class="time-sensitive-info-left">
+                            <h2>
+                                USE SMART TECHNOLOGY
+                            </h2>
+                            <p>
+                                Our time-sensitive deliveries are generally supplied with high-level smart technology which allows you to easily track parcels wherever they are. When order is intended to be delivered at a specified location at a certain time, this allows both you and
+                                the customer to guarantee the delivery process goes off without a hitch. You can learn more about our <a href="#">technology solutions here.</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--time-sensitive-box-end--->
+            </div>
+
+        </section>
+        <!--why skrillaz area end-->
+        <!--white area start-->
+        <section class="pink-area pt-100 pb-100">
+            <div class="container ">
+                <div class="col-xl-12">
+                    <div class="cta" data-aos="zoom-in" data-aos-duration="2000">
+                        <h1>Here at Skrillaz Logistics</h1>
+                        <p>we offer time-sensitive deliveries to our clients. If you would like to discuss individual requirements with a member of our experienced and friendly team, get in contact or request a quote from the website today.</p>
+                        <a href="#"><button>CONTACT US</button></a>
+                    </div>
+                </div>
+            </div>
+            <div class="cloud">
+                <img src="{{asset('frontend-asstets/assets/image/banner/cloud.svg')}}" alt="">
+            </div>
+            <div class="clouds">
+                <img src="{{asset('frontend-asstets/assets/image/banner/cloud.svg')}}" alt="">
+            </div>
+        </section>
+        <!--white area end-->
+
+    </main>
+    <!----------------------------------------main area end---------------------------------------->
+    @include('user.partials.footer')
+
+</body>
+
+</html>
